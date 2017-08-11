6e5f50913c943b56c6aa724f9be06b27c3bc1933
fanlu <fanlu7311@163.com>
2017-08-11 16:46:17 +0800

    fix

--- main.sh
+++ main.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 # set -x
-docker run --net=host --volume-driver=nvidia-docker --volume=nvidia_driver_375.20:/usr/local/nvidia:ro --device=/dev/nvidiactl --device=/dev/nvidia-uvm --device=/dev/nvidia-uvm-tools --device=/dev/nvidia0 --device=/dev/nvidia1 --device=/dev/nvidia2 --device=/dev/nvidia3 -e DMLC_PS_ROOT_URI=$DMLC_PS_ROOT_URI -e DMLC_PS_ROOT_PORT=$DMLC_PS_ROOT_PORT -e DMLC_NUM_WORKER=$DMLC_NUM_WORKER -e DMLC_NUM_SERVER=$DMLC_NUM_SERVER  -e DMLC_ROLE=$DMLC_ROLE -e DMLC_INTERFACE=ib0 -v /export/fanlu/deepspeech_zh_word_dist/:/export/fanlu/deepspeech_zh_word_dist/ -v /export/fanlu/aishell/:/export/fanlu/aishell/ -w /export/fanlu/deepspeech_zh_word_dist/ mxnet/python:gpu python main.py --configfile deepspeech.cfg
+docker run --net=host --volume-driver=nvidia-docker --volume=nvidia_driver_375.20:/usr/local/nvidia:ro \
+ --device=/dev/nvidiactl --device=/dev/nvidia-uvm --device=/dev/nvidia-uvm-tools \
+ --device=/dev/nvidia0 --device=/dev/nvidia1 --device=/dev/nvidia2 --device=/dev/nvidia3 \
+ -e DMLC_PS_ROOT_URI=$DMLC_PS_ROOT_URI -e DMLC_PS_ROOT_PORT=$DMLC_PS_ROOT_PORT -e DMLC_NUM_WORKER=$DMLC_NUM_WORKER \
+ -e DMLC_NUM_SERVER=$DMLC_NUM_SERVER  -e DMLC_ROLE=$DMLC_ROLE -e DMLC_INTERFACE=ib0 \
+ -v /export/fanlu/deepspeech_zh_word_dist/:/export/fanlu/deepspeech_zh_word_dist/ \
+ -v /export/fanlu/aishell/:/export/fanlu/aishell/ -w /export/fanlu/deepspeech_zh_word_dist/ \
+ mxnet/python:gpu python main.py --configfile deepspeech_dist.cfg
