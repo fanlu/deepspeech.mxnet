ce55a96185f042f8b23b9ca7d2b72fa8e9cb239b
fanlu <fanlu7311@163.com>
2017-09-05 22:25:13 +0800

    add num_hidden_proj fix

--- stt_layer_lstm.py
+++ stt_layer_lstm.py
@@ -216,8 +216,13 @@ def lstm_unroll(net, num_lstm_layer, seq_len, num_hidden_lstm_list, dropout=0., 
                 hidden_all.insert(0, hidden)
             else:
                 raise Exception("direction should be whether forward or backward")
-        net = hidden_all
-
+        # net = hidden_all
+        hidden_concat = mx.sym.Concat(*hidden_all, dim=1)
+        if num_hidden_proj > 0:
+            hidden_final = mx.sym.Reshape(hidden_concat, target_shape=(0, num_hidden_proj))
+        else:
+            hidden_final = mx.sym.Reshape(hidden_concat, target_shape=(0, num_hidden_lstm_list[-1]))
+        net = hidden_final
     return net
 
 
