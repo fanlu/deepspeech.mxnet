fc51f3c83c0304cc61575fdd08f9de01ec7cc3b3
fanlu <fanlu7311@163.com>
2017-09-15 17:10:34 +0800

    fix

--- cutfile.py
+++ cutfile.py
@@ -1,19 +1,21 @@
 # coding=utf-8
 import glob
+import os
+import time
+from multiprocessing import cpu_count
 
+import concurrent.futures
 from pydub import AudioSegment
 from pydub.silence import split_on_silence
-import random
-import sys
-from multiprocessing import cpu_count, Process, Manager
-import time
-import os
 
 max_duration = 16
 
 
 def deal_file(file_name, min_silence=500, silence_thresh=-50, result=[]):
-    sound = AudioSegment.from_wav(file_name)
+    if file_name[-3:] == "wav":
+        sound = AudioSegment.from_wav(file_name)
+    elif file_name[-3:] == "mp3":
+        sound = AudioSegment.from_mp3(file_name)
     info = "%s start pid: %s deal: %s" % (str(time.time()), str(os.getpid()), file_name)
     print(info)
     deal_chunk(sound, min_silence, silence_thresh, result)
@@ -41,46 +43,50 @@ if __name__ == "__main__":
 
     # file_name = "/Users/lonica/Downloads/7ebec23e-0d20-4e3d-afca-de325f7c2239.wav"
 
-    import concurrent.futures
-
-    files = glob.glob("/Users/lonica/Downloads/7*be*.wav")
+    files = glob.glob("/Users/lonica/Downloads/zt/00*.mp3")
 
     with concurrent.futures.ProcessPoolExecutor(max_workers=cpu_count() - 2) as executor:
-        for f, data in zip(files, executor.map(deal_file, files, min_silence=500, silence_thresh=-50, result=[])):
-            print('%r file return %d chunks' % (f, len(data)))
-            result = []
-            for d in data:
-                # 如果result最后一个chunk时间小于阈值
-                if len(result) > 0 and result[len(result) - 1].duration_seconds < 2:
-                    # 合并到result最后一条
-                    if d.duration_seconds + result[len(result) - 1].duration_seconds < max_duration:
-                        result[len(result) - 1] += d
+        future_to_f = {executor.submit(deal_file, f, min_silence=700, silence_thresh=-70, result=[]): f for f in files}
+        for future in concurrent.futures.as_completed(future_to_f):
+            f = future_to_f[future]
+            try:
+                data = future.result()
+                result = []
+                for d in data:
+                    # 如果result最后一个chunk时间小于阈值
+                    if len(result) > 0 and result[len(result) - 1].duration_seconds < 2:
+                        # 合并到result最后一条
+                        if d.duration_seconds + result[len(result) - 1].duration_seconds < max_duration:
+                            result[len(result) - 1] += d
+                        else:
+                            result.append(d)
                     else:
                         result.append(d)
-                else:
-                    result.append(d)
-            path, name = f.rsplit("/", 1)
-            for i, d in enumerate(result):
-                output_file = path + "/zt/500/" + name[:-4] + "_%03d.wav" % i
-                print(output_file, d.duration_seconds)
-                d.export(output_file, format="wav")
+                path, name = f.rsplit("/", 1)
+                for i, d in enumerate(result):
+                    output_file = path + "/500/" + name[:-4] + "_%03d.wav" % i
+                    print(output_file, d.duration_seconds)
+                    d.export(output_file, format="wav")
+            except Exception as exc:
+                print('%r generated an exception: %s' % (f, exc))
+            else:
+                print('%r file return %d chunks' % (f, len(data)))
+        # for f, data in zip(files, executor.map(deal_file, files, min_silence=500, silence_thresh=-50, result=[])):
+        #     print('%r file return %d chunks' % (f, len(data)))
+        #     result = []
+        #     for d in data:
+        #         # 如果result最后一个chunk时间小于阈值
+        #         if len(result) > 0 and result[len(result) - 1].duration_seconds < 2:
+        #             # 合并到result最后一条
+        #             if d.duration_seconds + result[len(result) - 1].duration_seconds < max_duration:
+        #                 result[len(result) - 1] += d
+        #             else:
+        #                 result.append(d)
+        #         else:
+        #             result.append(d)
+        #     path, name = f.rsplit("/", 1)
+        #     for i, d in enumerate(result):
+        #         output_file = path + "/zt/500/" + name[:-4] + "_%03d.wav" % i
+        #         print(output_file, d.duration_seconds)
+        #         d.export(output_file, format="wav")
 
-                # words = chunks[2:] #first and second are not words.
-
-                # len1 = len(words)
-                #
-                # new = AudioSegment.empty()
-                # silence = AudioSegment.silent(duration=1000)#1000ms silence
-                #
-                #
-                # order = range(len1)
-                # random.shuffle(order)
-                # print order
-                # comments = ""
-                #
-                # for i in order:
-                #     new += words[i]+silence
-                #     comments += str(i)+","
-                #
-                # save_name = file_name.split(".")[0]+"-random{0}.".format(random.randrange(0,9))+file_name.split(".")[1]
-                # new.export(save_name, format="mp3",tags={'artist': 'AppLeU0', 'album': file_name, 'comments': comments[:-1]})
