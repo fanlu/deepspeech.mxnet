cc37ade3fe93d2bfdf8609c999ec51279a2c9a62
fanlu <fanlu7311@163.com>
2017-09-21 18:15:32 +0800

    fix

--- main.sh
+++ main.sh
@@ -6,5 +6,6 @@ docker run --net=host --volume-driver=nvidia-docker --volume=nvidia_driver_375.2
  -e DMLC_PS_ROOT_URI=$DMLC_PS_ROOT_URI -e DMLC_PS_ROOT_PORT=$DMLC_PS_ROOT_PORT -e DMLC_NUM_WORKER=$DMLC_NUM_WORKER \
  -e DMLC_NUM_SERVER=$DMLC_NUM_SERVER  -e DMLC_ROLE=$DMLC_ROLE -e DMLC_INTERFACE=ib0 \
  -v /export/fanlu/deepspeech_zh_word_dist/:/export/fanlu/deepspeech_zh_word_dist/ \
+ -v /export/fanlu/thchs30/:/export/fanlu/thchs30/ -v /export/aiplatform/:/export/aiplatform/ \
  -v /export/fanlu/aishell/:/export/fanlu/aishell/ -w /export/fanlu/deepspeech_zh_word_dist/ \
- mxnet/python:gpu python main.py --configfile deepspeech_dist.cfg
+ dl/python python main.py --configfile deepspeech_dist.cfg
